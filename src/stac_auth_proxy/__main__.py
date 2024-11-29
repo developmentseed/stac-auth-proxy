3ba41451dbd4a624110cc4c0960363663b500e09
Anthony Lukach <anthonylukach@gmail.com>
2024-11-29 11:56:22 -0800

    chore: Refactor to utilize factory pattern for app

--- src/stac_auth_proxy/__main__.py
+++ src/stac_auth_proxy/__main__.py
@@ -8,9 +8,10 @@ LOGGING_CONFIG["loggers"][__package__] = {
 }
 
 uvicorn.run(
-    f"{__package__}.app:app",
+    f"{__package__}.app:create_app",
     host="0.0.0.0",
     port=8000,
     log_config=LOGGING_CONFIG,
     reload=True,
+    factory=True,
 )
--- src/stac_auth_proxy/app.py
+++ src/stac_auth_proxy/app.py
@@ -4,6 +4,8 @@ This module defines the FastAPI application for the STAC Auth Proxy, which handl
 authentication, authorization, and proxying of requests to some internal STAC API.
 """
 
+from typing import Optional
+
 from fastapi import Depends, FastAPI
 from fastapi.security import OpenIdConnect
 
@@ -11,34 +13,38 @@ from .proxy import Proxy
 from .config import Settings
 
 
-app = FastAPI()
-settings = Settings()
+def create_app(settings: Optional[Settings] = None) -> FastAPI:
+    settings = settings or Settings()
 
-open_id_connect_scheme = OpenIdConnect(
-    openIdConnectUrl=str(settings.oidc_discovery_url),
-    scheme_name="OpenID Connect",
-    description="OpenID Connect authentication for STAC API access",
-)
+    app = FastAPI()
 
-proxy = Proxy(upstream=str(settings.upstream_url))
-
-# Transactions Extension Endpoins
-for path, methods in {
-    # https://github.com/stac-api-extensions/collection-transaction/blob/v1.0.0-beta.1/README.md#methods
-    "/collections": ["POST"],
-    "/collections/{collection_id}": ["PUT", "PATCH", "DELETE"],
-    # https://github.com/stac-api-extensions/transaction/blob/v1.0.0-rc.3/README.md#methods
-    "/collections/{collection_id}/items": ["POST"],
-    "/collections/{collection_id}/items/{item_id}": ["PUT", "PATCH", "DELETE"],
-    # https://stac-utils.github.io/stac-fastapi/api/stac_fastapi/extensions/third_party/bulk_transactions/#bulktransactionextension
-    "/collections/{collection_id}/bulk_items": ["POST"],
-}.items():
-    app.add_api_route(
-        path,
-        proxy.passthrough,
-        methods=methods,
-        dependencies=[Depends(open_id_connect_scheme)],
+    open_id_connect_scheme = OpenIdConnect(
+        openIdConnectUrl=str(settings.oidc_discovery_url),
+        scheme_name="OpenID Connect",
+        description="OpenID Connect authentication for STAC API access",
     )
 
-# Catchall proxy
-app.add_route("/{path:path}", proxy.passthrough)
+    proxy = Proxy(upstream=str(settings.upstream_url))
+
+    # Transactions Extension Endpoins
+    for path, methods in {
+        # https://github.com/stac-api-extensions/collection-transaction/blob/v1.0.0-beta.1/README.md#methods
+        "/collections": ["POST"],
+        "/collections/{collection_id}": ["PUT", "PATCH", "DELETE"],
+        # https://github.com/stac-api-extensions/transaction/blob/v1.0.0-rc.3/README.md#methods
+        "/collections/{collection_id}/items": ["POST"],
+        "/collections/{collection_id}/items/{item_id}": ["PUT", "PATCH", "DELETE"],
+        # https://stac-utils.github.io/stac-fastapi/api/stac_fastapi/extensions/third_party/bulk_transactions/#bulktransactionextension
+        "/collections/{collection_id}/bulk_items": ["POST"],
+    }.items():
+        app.add_api_route(
+            path,
+            proxy.passthrough,
+            methods=methods,
+            dependencies=[Depends(open_id_connect_scheme)],
+        )
+
+    # Catchall proxy
+    app.add_route("/{path:path}", proxy.passthrough)
+
+    return app

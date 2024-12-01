38259f9a1f65d2dd562cf034791079af75b174c5
Anthony Lukach <anthonylukach@gmail.com>
2024-12-01 15:30:44 -0800

    Reorg tests, expand STAC API test serverr

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,26 +1,40 @@
 """Pytest fixtures."""
 
 import threading
+from typing import Optional
 
 import pytest
 import uvicorn
 from fastapi import FastAPI
 
-from stac_auth_proxy import Settings, create_app
-
 
 @pytest.fixture(scope="session")
 def source_api():
     """Create upstream API for testing purposes."""
-    app = FastAPI()
+    app = FastAPI(docs_url="/api.html", openapi_url="/api")
 
-    @app.get("/")
-    def read_root():
-        return {"message": "Hello from source API"}
-
-    @app.get("/items/{item_id}")
-    def read_item(item_id: int):
-        return {"item_id": item_id}
+    for path, methods in {
+        "/": ["GET"],
+        "/conformance": ["GET"],
+        "/queryables": ["GET"],
+        "/search": ["GET", "POST"],
+        "/collections": ["GET", "POST"],
+        "/collections/{collection_id}": ["GET", "PUT", "DELETE"],
+        "/collections/{collection_id}/items": ["GET", "POST"],
+        "/collections/{collection_id}/items/{item_id}": [
+            "GET",
+            "PUT",
+            "DELETE",
+        ],
+        "/collections/{collection_id}/bulk_items": ["POST"],
+    }.items():
+        for method in methods:
+            # NOTE: declare routes per method separately to avoid warning of "Duplicate Operation ID ... for function <lambda>"
+            app.add_api_route(
+                path,
+                lambda: {"id": f"Response from {method}@{path}"},
+                methods=[method],
+            )
 
     return app
 
@@ -41,16 +55,3 @@ def source_api_server(source_api):
     yield f"http://{host}:{port}"
     server.should_exit = True
     thread.join()
-
-
-@pytest.fixture
-def proxy_app(source_api_server: str) -> FastAPI:
-    """Fixture for the proxy app, pointing to the source API."""
-    test_app_settings = Settings(
-        upstream_url=source_api_server,
-        oidc_discovery_url="https://samples.auth0.com/.well-known/openid-configuration",
-        default_public=False,
-        _env_file=".env.test",
-    )
-    print(f"{test_app_settings=}")
-    return create_app(test_app_settings)

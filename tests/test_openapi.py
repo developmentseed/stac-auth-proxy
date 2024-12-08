d38c8b4c56553549f47b1771462c3b22d25735d5
Anthony Lukach <anthonylukach@gmail.com>
2024-12-07 20:41:57 -0800

    expand tests for complete coverage

--- tests/test_openapi.py
+++ tests/test_openapi.py
@@ -9,11 +9,29 @@ app_factory = AppFactory(
 )
 
 
-def test_no_edit_openapi_spec(source_api_server):
+def test_no_openapi_spec_endpoint(source_api_server):
     """When no OpenAPI spec endpoint is set, the proxied OpenAPI spec is unaltered."""
     app = app_factory(
         upstream_url=source_api_server,
         openapi_spec_endpoint=None,
+    )
+    client = TestClient(app)
+    response = client.get("/api")
+    assert response.status_code == 200
+    openapi = response.json()
+    assert "info" in openapi
+    assert "openapi" in openapi
+    assert "paths" in openapi
+    assert "oidcAuth" not in openapi.get("components", {}).get("securitySchemes", {})
+
+
+def test_no_private_endpoints(source_api_server):
+    """When no endpoints are private, the proxied OpenAPI spec is unaltered."""
+    app = app_factory(
+        upstream_url=source_api_server,
+        openapi_spec_endpoint="/api",
+        private_endpoints={},
+        default_public=True,
     )
     client = TestClient(app)
     response = client.get("/api")
@@ -45,7 +63,6 @@ def test_oidc_in_openapi_spec_private_endpoints(
     source_api: FastAPI, source_api_server: str
 ):
     """When OpenAPI spec endpoint is set & endpoints are marked private, those endpoints are marked private in the spec."""
-
     private_endpoints = {
         # https://github.com/stac-api-extensions/collection-transaction/blob/v1.0.0-beta.1/README.md#methods
         "/collections": ["POST"],
@@ -65,9 +82,14 @@ def test_oidc_in_openapi_spec_private_endpoints(
     openapi = client.get(source_api.openapi_url).raise_for_status().json()
     for path, methods in private_endpoints.items():
         for method in methods:
-            assert "oidcAuth" in (
-                openapi.get("paths", {})
-                .get(path, {})
-                .get(method, {})
-                .get("security", [])
-            )
+            openapi_path = openapi["paths"].get(path)
+            assert openapi_path, f"Path {path} not found in OpenAPI spec"
+            openapi_path_method = openapi_path.get(method.lower())
+            assert (
+                openapi_path_method
+            ), f"Method {method.lower()!r} not found for path {path!r} in OpenAPI spec for path {path}"
+            security = openapi_path_method.get("security")
+            assert security, f"Security not found for {path!r} {method.lower()!r}"
+            assert any(
+                "oidcAuth" in s for s in security
+            ), f'No "oidcAuth" in security for {path!r} {method.lower()!r}'

91f5e748aaf9a0b3b91ab26a13a05fbf0d651055
Anthony Lukach <anthonylukach@gmail.com>
2025-03-26 11:13:02 -0700

    refactor: simplify middleware util logic

--- src/stac_auth_proxy/utils/middleware.py
+++ src/stac_auth_proxy/utils/middleware.py
@@ -1,7 +1,6 @@
 """Utilities for middleware response handling."""
 
 import json
-import re
 from abc import ABC, abstractmethod
 from typing import Any, Optional
 
@@ -14,25 +13,20 @@ class JsonResponseMiddleware(ABC):
     """Base class for middleware that transforms JSON response bodies."""
 
     app: ASGIApp
-    json_content_type_expr: str = (
-        r"application/vnd\.oai\.openapi\+json;.*|application/json|application/geo\+json"
-    )
 
     @abstractmethod
-    def should_transform_response(self, request: Request) -> bool:
+    def should_transform_response(
+        self, request: Request, response_headers: Headers
+    ) -> bool:  # mypy: ignore
         """
-        Determine if this request's response should be transformed.
-
-        Args:
-            request: The incoming request
+        Determine if this response should be transformed. At a minimum, this
+        should check the request's path and content type.
 
         Returns
         -------
             bool: True if the response should be transformed
         """
-        return bool(
-            re.match(self.json_content_type_expr, request.headers.get("accept", ""))
-        )
+        ...
 
     @abstractmethod
     def transform_json(self, data: Any) -> Any:
@@ -46,36 +40,31 @@ class JsonResponseMiddleware(ABC):
         -------
             The transformed JSON data
         """
-        pass
+        ...
 
     async def __call__(self, scope: Scope, receive: Receive, send: Send) -> None:
         """Process the request/response."""
         if scope["type"] != "http":
             return await self.app(scope, receive, send)
 
-        request = Request(scope)
-        if not self.should_transform_response(request):
-            return await self.app(scope, receive, send)
-
         start_message: Optional[Message] = None
         body = b""
-        not_json = False
 
-        async def process_message(message: Message) -> None:
+        async def transform_response(message: Message) -> None:
             nonlocal start_message
             nonlocal body
-            nonlocal not_json
+
             if message["type"] == "http.response.start":
                 # Delay sending start message until we've processed the body
-                if not re.match(
-                    self.json_content_type_expr,
-                    Headers(scope=message).get("content-type", ""),
-                ):
-                    not_json = True
-                    return await send(message)
                 start_message = message
                 return
-            elif message["type"] != "http.response.body" or not_json:
+            assert start_message is not None
+            if not self.should_transform_response(
+                request=Request(scope),
+                response_headers=Headers(scope=start_message),
+            ):
+                return await send(message)
+            if message["type"] != "http.response.body":
                 return await send(message)
 
             body += message["body"]
@@ -109,4 +98,4 @@ class JsonResponseMiddleware(ABC):
                 }
             )
 
-        return await self.app(scope, receive, process_message)
+        return await self.app(scope, receive, transform_response)

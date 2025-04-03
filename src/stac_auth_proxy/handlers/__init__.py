d58336362eff158391127cc53b9dddb29c6edea1
Anthony Lukach <anthonylukach@gmail.com>
2025-04-02 21:51:56 -0700

    Rm asset signer endpoint

--- src/stac_auth_proxy/handlers/__init__.py
+++ src/stac_auth_proxy/handlers/__init__.py
@@ -2,6 +2,5 @@
 
 from .healthz import HealthzHandler
 from .reverse_proxy import ReverseProxyHandler
-from .s3_asset_signer import S3AssetSigner
 
-__all__ = ["ReverseProxyHandler", "HealthzHandler", "S3AssetSigner"]
+__all__ = ["ReverseProxyHandler", "HealthzHandler"]

f0ec9a58892d602315bb4724ebb7c273019f03a7
Anthony Lukach <anthonylukach@gmail.com>
2025-04-06 21:25:58 -0700

    fix: Rework body augmentor to avoid error on empty POSTs
    Was seeing `h11._util.LocalProtocolError: Too much data for declared Content-Length.`

--- src/stac_auth_proxy/middleware/ApplyCql2FilterMiddleware.py
+++ src/stac_auth_proxy/middleware/ApplyCql2FilterMiddleware.py
@@ -2,13 +2,12 @@
 
 import json
 import re
-from dataclasses import dataclass, field
-from functools import partial
+from dataclasses import dataclass
 from logging import getLogger
-from typing import Callable, Optional
+from typing import Optional
 
 from cql2 import Expr
-from starlette.datastructures import MutableHeaders, State
+from starlette.datastructures import MutableHeaders
 from starlette.requests import Request
 from starlette.types import ASGIApp, Message, Receive, Scope, Send
 
@@ -39,32 +38,25 @@ class ApplyCql2FilterMiddleware:
 
         request = Request(scope)
 
-        get_cql2_filter: Callable[[], Optional[Expr]] = partial(
-            getattr, request.state, self.state_key, None
-        )
+        cql2_filter: Optional[Expr] = getattr(request.state, self.state_key, None)
 
-        # Handle POST, PUT, PATCH
-        if request.method in ["POST", "PUT", "PATCH"]:
-            return await self.app(
-                scope,
-                Cql2RequestBodyAugmentor(
-                    receive=receive,
-                    state=request.state,
-                    get_cql2_filter=get_cql2_filter,
-                ),
-                send,
-            )
-
-        cql2_filter = get_cql2_filter()
         if not cql2_filter:
             return await self.app(scope, receive, send)
 
-        if re.match(r"^/collections/([^/]+)/items/([^/]+)$", request.url.path):
-            return await self.app(
-                scope,
-                receive,
-                Cql2ResponseBodyValidator(cql2_filter=cql2_filter, send=send),
+        # Handle POST, PUT, PATCH
+        if request.method in ["POST", "PUT", "PATCH"]:
+            req_body_handler = Cql2RequestBodyAugmentor(
+                app=self.app,
+                cql2_filter=cql2_filter,
             )
+            return await req_body_handler(scope, receive, send)
+
+        if re.match(r"^/collections/([^/]+)/items/([^/]+)$", request.url.path):
+            res_body_validator = Cql2ResponseBodyValidator(
+                app=self.app,
+                cql2_filter=cql2_filter,
+            )
+            return await res_body_validator(scope, send, receive)
 
         scope["query_string"] = filters.append_qs_filter(request.url.query, cql2_filter)
         return await self.app(scope, receive, send)
@@ -74,88 +66,115 @@ class ApplyCql2FilterMiddleware:
 class Cql2RequestBodyAugmentor:
     """Handler to augment the request body with a CQL2 filter."""
 
-    receive: Receive
-    state: State
-    get_cql2_filter: Callable[[], Optional[Expr]]
+    app: ASGIApp
+    cql2_filter: Expr
 
-    async def __call__(self) -> Message:
-        """Process a request body and augment with a CQL2 filter if available."""
-        message = await self.receive()
-        if message["type"] != "http.request":
-            return message
+    async def __call__(self, scope: Scope, receive: Receive, send: Send) -> None:
+        """Augment the request body with a CQL2 filter."""
+        body = b""
+        more_body = True
 
-        # NOTE: Can only get cql2 filter _after_ calling self.receive()
-        cql2_filter = self.get_cql2_filter()
-        if not cql2_filter:
-            return message
+        # Read the body
+        while more_body:
+            message = await receive()
+            if message["type"] == "http.request":
+                body += message.get("body", b"")
+                more_body = message.get("more_body", False)
 
+        # Modify body
         try:
-            body = json.loads(message.get("body", b"{}"))
+            body = json.loads(body)
         except json.JSONDecodeError as e:
             logger.warning("Failed to parse request body as JSON")
             # TODO: Return a 400 error
             raise e
 
-        new_body = filters.append_body_filter(body, cql2_filter)
-        message["body"] = json.dumps(new_body).encode("utf-8")
-        return message
+        # Augment the body
+        assert isinstance(body, dict), "Request body must be a JSON object"
+        new_body = json.dumps(
+            filters.append_body_filter(body, self.cql2_filter)
+        ).encode("utf-8")
+
+        # Patch content-length in the headers
+        headers = dict(scope["headers"])
+        headers[b"content-length"] = str(len(new_body)).encode("latin1")
+        scope["headers"] = list(headers.items())
+
+        async def new_receive():
+            return {
+                "type": "http.request",
+                "body": new_body,
+                "more_body": False,
+            }
+
+        await self.app(scope, new_receive, send)
 
 
 @dataclass
 class Cql2ResponseBodyValidator:
     """Handler to validate response body with CQL2."""
 
-    send: Send
+    app: ASGIApp
     cql2_filter: Expr
-    initial_message: Optional[Message] = field(init=False)
-    body: bytes = field(init=False, default_factory=bytes)
 
-    async def __call__(self, message: Message) -> None:
+    async def __call__(self, scope: Scope, send: Send, receive: Receive) -> None:
         """Process a response message and apply filtering if needed."""
-        if message["type"] == "http.response.start":
-            self.initial_message = message
-            return
+        if scope["type"] != "http":
+            return await self.app(scope, send, receive)
 
-        if message["type"] == "http.response.body":
-            assert self.initial_message, "Initial message not set"
+        body = b""
+        initial_message: Optional[Message] = None
 
-            self.body += message["body"]
+        async def _send_error_response(status: int, message: str) -> None:
+            """Send an error response with the given status and message."""
+            assert initial_message, "Initial message not set"
+            error_body = json.dumps({"message": message}).encode("utf-8")
+            headers = MutableHeaders(scope=initial_message)
+            headers["content-length"] = str(len(error_body))
+            initial_message["status"] = status
+            await send(initial_message)
+            await send(
+                {
+                    "type": "http.response.body",
+                    "body": error_body,
+                    "more_body": False,
+                }
+            )
+
+        async def buffered_send(message: Message) -> None:
+            """Process a response message and apply filtering if needed."""
+            nonlocal body
+            nonlocal initial_message
+
+            if message["type"] == "http.response.start":
+                initial_message = message
+                return
+
+            assert initial_message, "Initial message not set"
+
+            body += message["body"]
             if message.get("more_body"):
                 return
 
             try:
-                body_json = json.loads(self.body)
+                body_json = json.loads(body)
             except json.JSONDecodeError:
                 logger.warning("Failed to parse response body as JSON")
-                await self._send_error_response(502, "Not found")
+                await _send_error_response(502, "Not found")
                 return
 
             logger.debug(
                 "Applying %s filter to %s", self.cql2_filter.to_text(), body_json
             )
             if self.cql2_filter.matches(body_json):
-                await self.send(self.initial_message)
-                return await self.send(
+                await send(initial_message)
+                return await send(
                     {
                         "type": "http.response.body",
                         "body": json.dumps(body_json).encode("utf-8"),
                         "more_body": False,
                     }
                 )
-            return await self._send_error_response(404, "Not found")
+            return await _send_error_response(404, "Not found")
 
-    async def _send_error_response(self, status: int, message: str) -> None:
-        """Send an error response with the given status and message."""
-        assert self.initial_message, "Initial message not set"
-        error_body = json.dumps({"message": message}).encode("utf-8")
-        headers = MutableHeaders(scope=self.initial_message)
-        headers["content-length"] = str(len(error_body))
-        self.initial_message["status"] = status
-        await self.send(self.initial_message)
-        await self.send(
-            {
-                "type": "http.response.body",
-                "body": error_body,
-                "more_body": False,
-            }
-        )
+        return await self.app(scope, receive, buffered_send)

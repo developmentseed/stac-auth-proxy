4121700feb1aae3b1da7a7bdf4c7fe68287a9fde
Anthony Lukach <anthonylukach@gmail.com>
2025-04-08 19:35:48 -0700

    Proxy headers

--- src/stac_auth_proxy/handlers/reverse_proxy.py
+++ src/stac_auth_proxy/handlers/reverse_proxy.py
@@ -20,6 +20,9 @@ class ReverseProxyHandler:
     client: httpx.AsyncClient = None
     timeout: httpx.Timeout = field(default_factory=lambda: httpx.Timeout(timeout=15.0))
 
+    proxy_name: str = "stac-auth-proxy"
+    override_host: bool = True
+
     def __post_init__(self):
         """Initialize the HTTP client."""
         self.client = self.client or httpx.AsyncClient(
@@ -28,11 +31,37 @@ class ReverseProxyHandler:
             http2=True,
         )
 
+    def _prepare_headers(self, request: Request) -> MutableHeaders:
+        """Prepare headers for the proxied request."""
+        headers = MutableHeaders(request.headers)
+
+        # Handle null client host
+        client_host = request.client.host if request.client else "unknown"
+        proto = request.url.scheme
+
+        host = request.url.hostname
+        if request.url.port not in [80, 443]:
+            host = f"{host}:{request.url.port}"
+
+        headers.setdefault("X-Forwarded-For", client_host)
+        headers.setdefault("X-Forwarded-Host", request.url.hostname)
+        headers.setdefault("X-Forwarded-Proto", proto)
+        headers.setdefault(
+            "Forwarded",
+            f"for={client_host};host={host};proto={proto}",
+        )
+        headers.setdefault("Via", f"1.1 {self.proxy_name}")
+
+        if self.override_host:
+            headers["Host"] = self.client.base_url.netloc.decode("utf-8")
+            if self.client.base_url.port not in [80, 443]:
+                headers["Host"] = f'{headers["Host"]}:{request.url.port}'
+
+        return headers
+
     async def proxy_request(self, request: Request) -> Response:
         """Proxy a request to the upstream STAC API."""
-        headers = MutableHeaders(request.headers)
-        headers.setdefault("X-Forwarded-For", request.client.host)
-        headers.setdefault("X-Forwarded-Host", request.url.hostname)
+        headers = self._prepare_headers(request)
 
         # https://github.com/fastapi/fastapi/discussions/7382#discussioncomment-5136466
         rp_req = self.client.build_request(

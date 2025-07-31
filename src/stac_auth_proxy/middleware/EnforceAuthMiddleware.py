07b51cc27c393214025288154140d0552393210d
Anthony Lukach <anthonylukach@gmail.com>
2025-07-31 10:14:30 -0700

    fix: ensure OPTIONS requests are sent upstream without auth check

--- src/stac_auth_proxy/middleware/EnforceAuthMiddleware.py
+++ src/stac_auth_proxy/middleware/EnforceAuthMiddleware.py
@@ -85,6 +85,9 @@ class EnforceAuthMiddleware:
             return await self.app(scope, receive, send)
 
         request = Request(scope)
+        if request.method == "OPTIONS":
+            return await self.app(scope, receive, send)
+
         match = find_match(
             request.url.path,
             request.method,
@@ -148,7 +151,11 @@ class EnforceAuthMiddleware:
                 # NOTE: Audience validation MUST match audience claim if set in token (https://pyjwt.readthedocs.io/en/stable/changelog.html?highlight=audience#id40)
                 audience=self.allowed_jwt_audiences,
             )
-        except (jwt.exceptions.InvalidTokenError, jwt.exceptions.DecodeError) as e:
+        except (
+            jwt.exceptions.InvalidTokenError,
+            jwt.exceptions.DecodeError,
+            jwt.exceptions.PyJWKClientError,
+        ) as e:
             logger.error("InvalidTokenError: %r", e)
             raise HTTPException(
                 status_code=status.HTTP_401_UNAUTHORIZED,

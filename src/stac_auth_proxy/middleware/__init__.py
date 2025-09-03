2b2b22459c0e577b5a1d5d1e04c7de406d074a99
Anthony Lukach <anthonylukach@gmail.com>
2025-09-02 21:00:11 -0700

    feat: remove applied filters on response links (#67)
    closes #64

--- src/stac_auth_proxy/middleware/__init__.py
+++ src/stac_auth_proxy/middleware/__init__.py
@@ -5,6 +5,7 @@ from .AuthenticationExtensionMiddleware import AuthenticationExtensionMiddleware
 from .Cql2ApplyFilterBodyMiddleware import Cql2ApplyFilterBodyMiddleware
 from .Cql2ApplyFilterQueryStringMiddleware import Cql2ApplyFilterQueryStringMiddleware
 from .Cql2BuildFilterMiddleware import Cql2BuildFilterMiddleware
+from .Cql2RewriteLinksFilterMiddleware import Cql2RewriteLinksFilterMiddleware
 from .Cql2ValidateResponseBodyMiddleware import Cql2ValidateResponseBodyMiddleware
 from .EnforceAuthMiddleware import EnforceAuthMiddleware
 from .ProcessLinksMiddleware import ProcessLinksMiddleware
@@ -17,6 +18,7 @@ __all__ = [
     "Cql2ApplyFilterBodyMiddleware",
     "Cql2ApplyFilterQueryStringMiddleware",
     "Cql2BuildFilterMiddleware",
+    "Cql2RewriteLinksFilterMiddleware",
     "Cql2ValidateResponseBodyMiddleware",
     "EnforceAuthMiddleware",
     "OpenApiMiddleware",

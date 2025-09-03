c8940260cbe69bdc7868f16f5c8a76f9ae29b9d6
Anthony Lukach <anthonylukach@gmail.com>
2025-09-03 09:15:50 -0700

    feat: make use of Server-Timing header
    closes #69

--- src/stac_auth_proxy/middleware/AddProcessTimeHeaderMiddleware.py
+++ src/stac_auth_proxy/middleware/AddProcessTimeHeaderMiddleware.py
@@ -1,4 +1,4 @@
-"""Middleware to add a header with the process time to the response."""
+"""Middleware to add Server-Timing header with proxy processing time."""
 
 import time
 
@@ -7,12 +7,25 @@ from starlette.middleware.base import BaseHTTPMiddleware
 
 
 class AddProcessTimeHeaderMiddleware(BaseHTTPMiddleware):
-    """Middleware to add a header with the process time to the response."""
+    """Middleware to add Server-Timing header with proxy processing time."""
 
     async def dispatch(self, request: Request, call_next) -> Response:
-        """Add a header with the process time to the response."""
+        """Add Server-Timing header with proxy processing time to the response."""
         start_time = time.perf_counter()
         response = await call_next(request)
         process_time = time.perf_counter() - start_time
-        response.headers["X-Process-Time"] = f"{process_time:.3f}"
+
+        # Add Server-Timing header with proxy processing time
+        # Format: Server-Timing: proxy;dur=123.456
+        server_timing_value = f"proxy;dur={process_time:.3f}"
+
+        # If there's already a Server-Timing header, append to it
+        existing_timing = response.headers.get("Server-Timing")
+        if existing_timing:
+            response.headers["Server-Timing"] = (
+                f"{existing_timing}, {server_timing_value}"
+            )
+        else:
+            response.headers["Server-Timing"] = server_timing_value
+
         return response

9a18c49f74695dfdde516f6554a6bb6f6244937c
Anthony Lukach <anthonylukach@gmail.com>
2025-10-14 09:16:46 -0700

    feat: support customizing port when running as a module

--- src/stac_auth_proxy/__main__.py
+++ src/stac_auth_proxy/__main__.py
@@ -1,12 +1,14 @@
 """Entry point for running the module without customized code."""
 
+import os
+
 import uvicorn
 from uvicorn.config import LOGGING_CONFIG
 
 uvicorn.run(
     f"{__package__}.app:create_app",
     host="0.0.0.0",
-    port=8000,
+    port=int(os.getenv("PORT", 8000)),
     log_config={
         **LOGGING_CONFIG,
         "loggers": {

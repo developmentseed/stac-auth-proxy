504074f9bf5cb17129ad38261d5bb499daced8b4
Anthony Lukach <anthonylukach@gmail.com>
2026-02-19 13:24:21 -0800

    fix(jinja2): use sandboxed environment

--- src/stac_auth_proxy/filters/template.py
+++ src/stac_auth_proxy/filters/template.py
@@ -3,7 +3,8 @@
 from dataclasses import dataclass, field
 from typing import Any
 
-from jinja2 import BaseLoader, Environment
+from jinja2 import BaseLoader
+from jinja2.sandbox import SandboxedEnvironment
 
 
 @dataclass
@@ -11,11 +12,13 @@ class Template:
     """Generate CQL2 filter expressions via Jinja2 templating."""
 
     template_str: str
-    env: Environment = field(init=False)
+    env: SandboxedEnvironment = field(init=False)
 
     def __post_init__(self):
         """Initialize the Jinja2 environment."""
-        self.env = Environment(loader=BaseLoader).from_string(self.template_str)
+        self.env = SandboxedEnvironment(loader=BaseLoader).from_string(
+            self.template_str
+        )
 
     async def __call__(self, context: dict[str, Any]) -> str:
         """Render a CQL2 filter expression with the request and auth token."""
